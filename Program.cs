767756316b9ebe4b88668050421fda05b1eabf0a
oleg-berger <126603656+oleg-berger@users.noreply.github.com>
2023-12-01 22:54:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.Write("Введите числа через запятую без пробелов ");
+string inputStr = Console.ReadLine();
+string[] text = inputStr.Split(",");
+string compareText = "";
+
+foreach (string item in text)
+{
+    Console.Write(item);
+}
+
+for (int i = text.Length - 1; i >= 0; i--)
+{
+    compareText += text[i];
+}
+ Console.WriteLine(" " + compareText);
+
+string arrayToStr = "";
+
+for (int j =  0; j < text.Length; j++)
+{
+    arrayToStr += text[j];
+}
+
+
+
+if (compareText == arrayToStr)
+{
+    Console.WriteLine("True");
+}
+else 
+{
+    Console.WriteLine("False");
+}
+
